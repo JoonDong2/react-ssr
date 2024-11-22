07db99615a0d16b37cbf92ac72da0ed6c9995824
Joondong <kwonjoondong@gmail.com>
2024-11-22 14:50:26 +0900

    featt: server app

--- server/index.tsx
+++ server/index.tsx
@@ -1,14 +1,53 @@
-import path from 'path'
-import fs from 'fs';
-import express from 'express';
+import React, { Suspense } from "react";
+import path from "path";
+import fs from "fs";
+import express from "express";
+import { renderToPipeableStream } from "react-dom/server";
+import App from "../src/App";
+import { Suspender } from "../src/suspender";
+import { Writable } from "stream";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  console.log('hi')
-  res.sendStatus(200)
-})
+const template = fs.readFileSync(path.resolve("./src/index.html"), "utf-8");
+const [header, footer] = template.split("{{CONTENTS}}");
+
+app.use(express.static("build/client"));
+
+app.get("/", (req, res) => {
+  const key = String(req.query.key);
+  const { pipe } = renderToPipeableStream(
+    <Suspense fallback="loading...">
+      <App queryKey={key} />
+    </Suspense>,
+    {
+      onShellReady() {
+        res.write(header);
+        const passThough = new Writable({
+          write(chunk, encodong, callback) {
+            res.write(chunk);
+            callback();
+          },
+          final(callback) {
+            // 추가 데이터
+            const stringifiedSuspenderCache = JSON.stringify(
+              Suspender.values()
+            );
+            res.write(
+              `<script>window.__SUSPENDER_CACHE__=${stringifiedSuspenderCache}</script>`
+            );
+            res.end();
+            callback();
+          },
+        });
+        pipe(passThough);
+
+        res.write(footer);
+      },
+    }
+  );
+});
 
 app.listen(3000, () => {
-  console.log('good')
-})
+  console.log("server start !");
+});
